5cd0b7164ef45a7bd1e408444968e27c56b965ef
visionmedia <tj@vision-media.ca>
2009-07-14 16:52:01 -0700

    Started library

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,8 +1,14 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'jQuery'
+  describe '.search()'
+    it 'should filter'
+      users = elements(fixture('users'))
+      items = users.children('li')
+      $.search(items, 'Holowaychuk')
+      items.get(0).should.be_visible
+      items.get(1).should.be_visible
+      items.get(2).should.be_hidden
+      items.get(3).should.be_hidden
     end
   end
 end
