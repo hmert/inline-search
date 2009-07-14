64beddb48be28aa7e41b3efa9c5ed4bba9d28c31
visionmedia <tj@vision-media.ca>
2009-07-14 16:58:43 -0700

    Added "by keyword" filter

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,10 +1,21 @@
 
 describe 'jQuery'
   describe '.search()'
-    it 'should filter'
+    before_each
       users = elements(fixture('users'))
       items = users.children('li')
-      $.search(items, 'Holowaychuk')
+    end
+    
+    it 'should filter elements by substring by default'
+      $.search(items, 'Holoway')
+      items.get(0).should.be_visible
+      items.get(1).should.be_visible
+      items.get(2).should.be_hidden
+      items.get(3).should.be_hidden
+    end
+        
+    it 'should filter elements by keywords'
+      $.search(items, 'Holowaychuk', 'by keyword')
       items.get(0).should.be_visible
       items.get(1).should.be_visible
       items.get(2).should.be_hidden
