816ea6e49c740eb6ccbb7d9afe7898400c527d9b
visionmedia <tj@vision-media.ca>
2009-07-14 17:02:07 -0700

    More specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -6,20 +6,33 @@ describe 'jQuery'
       items = users.children('li')
     end
     
-    it 'should filter elements by substring by default'
-      $.search(items, 'Holoway')
-      items.get(0).should.be_visible
-      items.get(1).should.be_visible
-      items.get(2).should.be_hidden
-      items.get(3).should.be_hidden
+    describe 'filter'
+      describe 'by substring'
+        it 'should be the default'
+          $.search(items, 'Holoway')
+          items.get(0).should.be_visible
+          items.get(1).should.be_visible
+          items.get(2).should.be_hidden
+          items.get(3).should.be_hidden 
+        end
+      end
+      
+      describe 'by keyword'
+        it 'should filter by entire words only'
+          $.search(items, 'Holowaychuk', 'by keyword')
+          items.get(0).should.be_visible
+          items.get(1).should.be_visible
+          items.get(2).should.be_hidden
+          items.get(3).should.be_hidden
+          
+          $.search(items, 'Holoway', 'by keyword')
+          items.get(0).should.be_visible
+          items.get(1).should.be_visible
+          items.get(2).should.be_visible
+          items.get(3).should.be_visible
+        end
+      end
     end
-        
-    it 'should filter elements by keywords'
-      $.search(items, 'Holowaychuk', 'by keyword')
-      items.get(0).should.be_visible
-      items.get(1).should.be_visible
-      items.get(2).should.be_hidden
-      items.get(3).should.be_hidden
-    end
+    
   end
 end
