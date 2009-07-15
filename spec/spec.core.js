c75a643bb054983027a45b6a49196fda8a4a50c6
visionmedia <tj@vision-media.ca>
2009-07-15 09:37:19 -0700

    Fixed spec nesting

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -55,53 +55,53 @@ describe 'jQuery'
         end
       end
     end
+  end
+  
+  describe '.fn.search()'
+    before_each
+      table = $(fixture('vehicles'))
+      th = table.find('th')
+      skyline = table.find('tr:nth-child(1)')
+      rx7 = table.find('tr:nth-child(2)')
+      supra = table.find('tr:nth-child(3)')
+    end
     
-    describe '.fn.search()'
-      before_each
-        table = $(fixture('vehicles'))
-        th = table.find('th')
-        skyline = table.find('tr:nth-child(1)')
-        rx7 = table.find('tr:nth-child(2)')
-        supra = table.find('tr:nth-child(3)')
-      end
-      
-      it 'should work'
-        $(items).search('Holoway')
-        items.get(0).should.be_visible
-        items.get(1).should.be_visible
-        items.get(2).should.be_hidden
-        items.get(3).should.be_hidden
-      end
-      
-      it 'should work with other filters'
-        $(items).search('Alan Taylor', 'by keyword')
-        items.get(0).should.be_hidden
-        items.get(1).should.be_visible
-        items.get(2).should.be_visible
-        items.get(3).should.be_hidden
-      end
-      
-      it 'should allow a function to determine what should be filtered'
-        parent = function(){ return this.parent() }
-        $('tr td:nth-child(1)', table).search('Toyota', 'by keyword', { remove : parent })
-        th.should.be_visible
-        skyline.should.be_hidden
-        rx7.should.be_hidden
-        supra.should.be_visible
-      end
-      
-      describe 'lookup functions'
-        describe 'parent'
-          it 'should remove the filtered elements parent'
-            $('tr td:nth-child(1)', table).search('Toyota', 'by keyword', { remove : 'parent' })
-            th.should.be_visible
-            skyline.should.be_hidden
-            rx7.should.be_hidden
-            supra.should.be_visible
-          end
+    it 'should work'
+      $(items).search('Holoway')
+      items.get(0).should.be_visible
+      items.get(1).should.be_visible
+      items.get(2).should.be_hidden
+      items.get(3).should.be_hidden
+    end
+    
+    it 'should work with other filters'
+      $(items).search('Alan Taylor', 'by keyword')
+      items.get(0).should.be_hidden
+      items.get(1).should.be_visible
+      items.get(2).should.be_visible
+      items.get(3).should.be_hidden
+    end
+    
+    it 'should allow a function to determine what should be filtered'
+      parent = function(){ return this.parent() }
+      $('tr td:nth-child(1)', table).search('Toyota', 'by keyword', { remove : parent })
+      th.should.be_visible
+      skyline.should.be_hidden
+      rx7.should.be_hidden
+      supra.should.be_visible
+    end
+    
+    describe 'lookup functions'
+      describe 'parent'
+        it 'should remove the filtered elements parent'
+          $('tr td:nth-child(1)', table).search('Toyota', 'by keyword', { remove : 'parent' })
+          th.should.be_visible
+          skyline.should.be_hidden
+          rx7.should.be_hidden
+          supra.should.be_visible
         end
       end
     end
-    
   end
+  
 end
