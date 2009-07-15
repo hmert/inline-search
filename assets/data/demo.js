208d1eb388bda8df1dc59d951490f128fd602966
visionmedia <tj@vision-media.ca>
2009-07-15 10:23:53 -0700

    More demos

--- assets/data/demo.js
+++ assets/data/demo.js
@@ -9,4 +9,12 @@ $(function(){
       return !matches.length
     })
   })
+  
+  $('input[name=keywords2]').keyup(function(){
+    $('ul.users li').search($(this).val())
+  })
+  
+  $('input[name=keywords3]').keyup(function(){
+    $('ul.users2 li').search($(this).val(), 'by keyword')
+  })
 })
